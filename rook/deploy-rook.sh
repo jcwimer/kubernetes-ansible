0537239d74074869910ce9ae091c44746c05ce7f
Jacob Cody Wimer <jacob.wimer@gmail.com>
2019-05-03 15:09:54 -0400

    Changed rooke to standard storageclass

--- rook/deploy-rook.sh
+++ rook/deploy-rook.sh
@@ -2,5 +2,5 @@ kubectl apply -f operator.yaml
 sleep 30s
 kubectl apply -f cluster.yaml
 sleep 60s
-echo kubectl apply -f storageclass.yaml
 kubectl apply -f dashboard-external.yaml
+echo kubectl apply -f storageclass.yaml
